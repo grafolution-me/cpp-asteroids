a4beff33be0b28b2cd0154e1077a527bcb512dac
checkfelix123 <graf.felix@aon.at>
2021-03-13 00:58:46 +0100

    removing dead code in shape.h, triangle.h, draw_window.h

--- asteriods/src/shape.h
+++ asteriods/src/shape.h
@@ -13,12 +13,12 @@ public:
 		m_brush{ brush },
 		m_pen{ pen },
 		m_box{ box } {
-	}	explicit shape(const wxRect& box, const	std::array<wxRealPoint, 3> m_points, wxPoint middle, const wxPen& pen, const wxBrush& brush) :
+	}	
+	explicit shape(const wxRect& box, const	std::array<wxRealPoint, 3> m_points, const wxPen& pen, const wxBrush& brush) :
 		m_brush{ brush },
 		m_pen{ pen },
 		m_box{ box },
-		m_points{ m_points },
-		m_middle{ middle }
+		m_points{ m_points }
 	{
 	}
 
@@ -30,34 +30,34 @@ public:
 	wxRealPoint& rotate(wxRealPoint p, double Cx, double Cy, int angle) {
 		double	Rx = (Cx * cos(angle * (M_PI / 180)) - (Cy * sin(angle * (M_PI / 180))));
 		double	Ry = (Cx * sin(angle * (M_PI / 180)) + (Cy * cos(angle * (M_PI / 180))));
-
 		wxRealPoint  p_return(std::round(Rx) + p.x, std::round(Ry) + p.y) ;
 		return p_return;
 	}
-	void move(const int offset) {
-
+	wxRealPoint& get_center_shape(std::vector<wxRealPoint>v) {
 		double center_x = 0;
 		double center_y = 0;
-		for (size_t i = 0; i < m_points.size(); i++)
+		for (wxRealPoint point : v)
 		{
-			center_x += m_points[i].x;
-			center_y += m_points[i].y;
-
+			center_x += point.x;
+			center_y += point.y;
 		}
-		wxRealPoint center{ center_x / 3, center_y / 3 };
+		wxRealPoint center{ center_x / v.size(), center_y / v.size() };
+		return center;
+	}
+	void move(const int offset) {
+		wxRealPoint center = get_center_shape(std::vector<wxRealPoint>{ std::begin(m_points), std::end(m_points) });
 		for (size_t i = 0; i < m_points.size(); i++)
 		{
 			wxRealPoint position = m_points[i];
 			m_points[i] = rotate(center, position.x - center.x, position.y - center.y, offset);
 		}
+		set_rotation(offset);
+	}
+	
+	int get_rotation() {
+		return rotation;
 	}
 	void move_x(const int offset) {
-		//for (size_t i = 0; i < m_points.size(); i++)
-		//{
-		//	wxPoint position = m_points[i]-x + offset;
-		//	
-		//}
-
 	}
 	int get_left() {
 		return m_box.GetLeft();
@@ -75,10 +75,14 @@ public:
 
 
 protected:
+	void set_rotation(const int offset) {
+		rotation += offset;
+	}
 	wxBrush m_brush{};
 	wxPen m_pen{};
 	wxRect m_box{};
 	std::array<wxRealPoint, 3> m_points{};
-	wxPoint m_middle{};
 	virtual void do_draw(context_t& context) const = 0;
+private:
+	int rotation;
 };
